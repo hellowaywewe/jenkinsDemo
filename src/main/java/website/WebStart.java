8d723fcecd939a5355d43ad9de2ec7bbaeadb4ff
helloway <simple_hlw@163.com>
2020-08-31 09:29:34 +0800

    add some code

--- src/main/java/website/WebStart.java
+++ src/main/java/website/WebStart.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebStart {
     public static void main(String[] args) {
+        System.out.println("WebStart!!");
         SpringApplication.run(WebStart.class, args);
     }
 }
